5dad660c026749dc3618350c8efda5f38283d781
buckyinsfo <buckyinsfo@gmail.com>
2023-06-21 12:06:48 -0700

    add --oi;lenssl-legacy-rovider switch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useLayoutEffect, useState } from 'react'
+//import rough from 'roughjs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
 
-export default App;
+    const [count, setCount] = useState(0);
+
+    // During first render
+    // function Counter() {
+    //     const count = 0; // Returned by useState()
+    // }
+
+    function handleAlertClick() {
+        setTimeout(() => {
+            alert('You clicked on: ' + count);
+        }, 3000);
+    }
+
+    // check to see if this is the hook you use to manipulate dom elements?
+    useLayoutEffect(() => {
+        const canvas = document.getElementById("canvas")
+        const ctx = canvas.getContext("2d")
+
+        ctx.fillStyle = "blue"
+        ctx.fillRect(0, 0, 550, 550)
+
+        //const roughCanvas = rough.canvas(document.getElementById("canvas"))
+        //roughCanvas.rectangle(100, 100, 400, 400)
+    })
+
+    return (
+        <dev>
+            <canvas id="canvas" width={window.innerWidth} height={window.innerHeight / 2}>Canvas</canvas>
+            <button onClick={() => setCount(count + 1)}>Click Me! {count}</button>
+            <button onClick={handleAlertClick}>Show Alert</button>
+    </dev >)
+          
+};
+
+export default App;    
+
+// {/* <button onClick={() => setCount(count + 1)}>
+//             Click me
+//          </button> */}
