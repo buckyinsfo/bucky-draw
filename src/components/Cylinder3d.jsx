8a9aebb101fa7ef11389993b9c457f4a72cf53b2
buckyinsfo <buckyinsfo@gmail.com>
2023-07-03 06:16:52 -0700

    Run Prettier

--- src/components/Cylinder3d.jsx
+++ src/components/Cylinder3d.jsx
@@ -1,17 +1,20 @@
-import React, { useRef, useState } from "react"
-import { useFrame } from "@react-three/fiber"
- 
-const Cylinder3d = (props) => {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.z += 0.01))
-  // Return the view, these are regular Threejs elements expressed in JSX
+import React, { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
 
-        return (
+const Cylinder3d = (props) => {
+    // This reference gives us direct access to the THREE.Mesh object
+    const ref = useRef();
+
+    // Hold state for hovered and clicked events
+    const [hovered, hover] = useState(false);
+    const [clicked, click] = useState(false);
+  
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (ref.current.rotation.z += 0.01));
+
+    // Return the view, these are regular Threejs elements expressed in JSX
+
+    return (
         <mesh
             {...props}
             ref={ref}
@@ -20,15 +23,14 @@ const Cylinder3d = (props) => {
             onPointerOver={(event) => hover(true)}
             onPointerOut={(event) => hover(false)}
             >
-  
             <cylinderGeometry args={[1, 1, 1]} />
-  
+
             <meshStandardMaterial
                 wireframe={props.wireframe}
                 color={hovered ? "hotpink" : "orange"}
-            />
+        />
         </mesh>
-    )
+  )
 }
- 
+
 export default Cylinder3d;
