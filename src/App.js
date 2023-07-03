4afd06d20550955cd9501ed407b4c6381321227e
buckyinsfo <buckyinsfo@gmail.com>
2023-07-03 06:08:27 -0700

    Add react fiber and three libraries

--- src/App.js
+++ src/App.js
@@ -1,44 +1,65 @@
-import React, { useLayoutEffect, useState } from 'react'
-//import rough from 'roughjs'
+import React, { Suspense } from 'react'
+import { Canvas } from "@react-three/fiber"
+import Cylinder3d from './components/Cylinder3d'
+import Square0 from './components/Square0.jsx'
+import './style.css'
+
+import { PerspectiveCamera, OrbitControls } from '@react-three/drei'
+
+const Carshow = () => { 
+    return (
+        <>
+            <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+            <PerspectiveCamera makedefault fov={50} position={[3, 2, 5]} />
+            
+            <ambientLight intensity={0.1} />
+            <directionalLight color="red" position={[0, 0, 5]} />
+
+            <mesh>
+                <boxGeometry args={[2, 2, 2]} />
+                <meshBasicMaterial color={'blue'} />
+            </mesh>
+        </>
+    )
+};
+
+const Stack = () => {
+    return (
+        <section className='App-header'>
+            {/* Canvas 1 */}
+            <Canvas>
+                <pointLight position={[10, 10, 10]} />
+                <ambientLight />
+                <Cylinder3d position={[-1.2, 0, 0]} />
+                <Cylinder3d position={[1.2, 0, 0]} />
+            </Canvas>
+    
+            {/* Canvas 2 */}
+            <Canvas>
+                <pointLight position={[10, 10, 10]} />
+                <ambientLight intensity={0.5} />
+                <Cylinder3d position={[-1.2, 0, 0]} wireframe={true} />
+                <Cylinder3d position={[1.2, 0, 0]} wireframe={true} />
+            </Canvas>
+    
+            {/* Canvas 3 */}
+            <Canvas>
+                <pointLight position={[10, 10, 10]} />
+                <ambientLight color={"red"} />
+                <Cylinder3d position={[-1.2, 0, 0]} />
+                <Cylinder3d position={[1.2, 0, 0]} />
+            </Canvas>
+        </section>
+    )
+}
 
 const App = () => {
-
-    const [count, setCount] = useState(0);
-
-    // During first render
-    // function Counter() {
-    //     const count = 0; // Returned by useState()
-    // }
-
-    function handleAlertClick() {
-        setTimeout(() => {
-            alert('You clicked on: ' + count);
-        }, 3000);
-    }
-
-    // check to see if this is the hook you use to manipulate dom elements?
-    useLayoutEffect(() => {
-        const canvas = document.getElementById("canvas")
-        const ctx = canvas.getContext("2d")
-
-        ctx.fillStyle = "blue"
-        ctx.fillRect(0, 0, 550, 550)
-
-        //const roughCanvas = rough.canvas(document.getElementById("canvas"))
-        //roughCanvas.rectangle(100, 100, 400, 400)
-    })
-
     return (
-        <dev>
-            <canvas id="canvas" width={window.innerWidth} height={window.innerHeight / 2}>Canvas</canvas>
-            <button onClick={() => setCount(count + 1)}>Click Me! {count}</button>
-            <button onClick={handleAlertClick}>Show Alert</button>
-    </dev >)
-          
+        <>
+            <Square0 />
+        </>
+    )
+    
 };
 
 export default App;    
-
-// {/* <button onClick={() => setCount(count + 1)}>
-//             Click me
-//          </button> */}
---
+++ src/components/Square0.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import * as THREE from 'three'
+// import OrbitControls from 'three-orbitcontrols'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+
+
+class Square0 extends Component {
+    componentDidMount() {
+        // scene
+        this.scene = new THREE.Scene()
+
+        // renderer
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize( window.innerWidth, window.innerHeight )
+        
+        this.mount.appendChild( this.renderer.domElement )
+
+        // camera
+        this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 10000 )
+        this.camera.position.z=5
+
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement )
+
+        // Create Box
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 )
+        const material = new THREE.MeshBasicMaterial({ color: 0x0ff00 , wireframe: true  })
+        this.cube = new THREE.Mesh( geometry, material )
+        
+        // Add to Scene
+        this.scene.add( this.cube )
+        this.animation()
+        this.renderer.render( this.scene, this.camera )
+
+        // Event Listener
+        window.addEventListener( 'resize', this.handleWindowResize )
+    }
+
+    animation = ()  => {
+        requestAnimationFrame( this.animation )
+        this.controls.update()
+
+        this.cube.rotation.x +=0.01
+        this.cube.rotation.y +=0.01
+        this.renderer.render( this.scene, this.camera )
+    }
+
+    handleWindowResize = () => {
+        this.camera.aspect = window.innerWidth / window.innerHeight
+        this.camera.updateProjectionMatrix()
+        
+        this.renderer.setSize( window.innerWidth, window.innerHeight )
+        this.renderer.render( this.scene, this.camera )
+    }
+
+    render() {
+        return (
+            <div ref={ mount => { this.mount = mount }} />
+        )
+    }
+}
+
+export default Square0;
+
+
+    // var square = new THREE.Geometry()
+	// square.vertices.push( new THREE.Vector3( x1, y1, 0 ) )
+	// square.vertices.push( new THREE.Vector3( x2, y1, 0 ) )
+	// square.vertices.push( new THREE.Vector3( x2, y2, 0 ) )
+	// square.vertices.push( new THREE.Vector3( x1, y2, 0 ) )
+	// square.faces.push( new THREE.Face3( 0, 1, 2 ) )
+	// square.faces.push( new THREE.Face3( 2, 0, 3 ) )
+	// return square
